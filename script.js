e6b1cd9b416ee07f1cff3da6494903df11ae56cd
lianaschapiro <liana.schapiro@gmail.com>
2015-12-12 13:22:39 -0500

    Added a 'confirm' popup for when you try to delete items off the list

--- script.js
+++ script.js
@@ -4,25 +4,19 @@ $(document).ready(function() {
     	e.preventDefault();
     	var text = $("input").val();
     	$("ul").append("<li>" + text + "</li>");
-
     	$("#itemDescription").val("");
-
-		
-		
-
-		$('li').bind('dblclick', function(){
-			// var complete = confirm("Are you sure you want to remove this item?");
-		 //    if (complete == true){
-		 //    	return false;
-		 //    } else {
-		    	$(this).remove();
-		    // }
-
-		});
-    });
+  	 });
 
     $('ul').on('click', 'li', function(){
 	    $(this).toggleClass('complete');
-	
 	});
+
+$('ul').on('dblclick', "li", function(){
+			var complete = confirm("Are you sure you want to remove this item?");
+		    if (complete == true){
+		    	$(this).remove();
+		    } else {
+		    	return false;
+		    }
+		});
 });
